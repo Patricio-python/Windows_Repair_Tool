abc1a640e8003c74d78f76a2ffec805e33571402
Luiz Patricio <patricio.python@gmail.com>
2026-04-15 18:40:50 -0300

    Fix formatting and improve messages in batch file

--- Windows_Repair_Tool.bat
+++ Windows_Repair_Tool.bat
@@ -1,10 +1,10 @@
 @echo off
 net session >nul 2>&1
 if %errorLevel% == 0 (
-    echo privilégio de Administrador Confirmado.
+    echo privilegio de Administrador Confirmado.
 ) else (
-    echo !!Use Privilégio de administrador para usar essa ferramenta!!
-    echo Clique com o botão direito do mouse nesse arquivo e selecione 'Executar como Administrador'.
+    echo !!Use Privilegio de administrador para usar essa ferramenta!!
+    echo Clique com o botao direito do mouse nesse arquivo e selecione 'Executar como Administrador'.
     pause
     exit /b
 )
@@ -12,7 +12,7 @@ echo Limpando arquivos antigos e liberando espaço...
 dism /online /cleanup-image /startcomponentcleanupecho.
 echo.
 echo Executando DISM - RestoreHealth.
-echo Reparando possíveis corrupções na imagem do sistema...
+echo Reparando possiveis corrupcoes na imagem do sistema...
 dism /online /cleanup-image /restorehealth
 echo.
 echo Executando System File Checker.
